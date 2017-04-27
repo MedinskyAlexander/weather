149a4b27f0d63e03b0686e6bdc85651789555396
Alexander Medinsky <a.medinsky@insart.com>
2017-04-27 19:44:49 +0300

    weather info implementing started

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,9 +2,22 @@
 
 angular.module('WeatherViewer').controller('WeatherController', ['$scope', 'WeatherService', function ($scope, WeatherService) {
 
+    $scope.cityInfo = {};
+    $scope.showDetails = false;
+
     $scope.getWeather = function (city) {
         WeatherService.getWeather(city).then(function (data) {
+            if (data) {
+                $scope.cityInfo = data.current_observation;
+                $scope.showDetails = true;
+//                display_location.city
+//                display_location.country
+//                display_location.full!!!!
+//                display_location.latitude
+//                display_location.longitude
+            }
         });
+//        todo: incorrect city name handling
     }
 
 }]);
